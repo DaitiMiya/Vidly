0fd9d8864532ec76d8c2378267497854dc2fc70c
Daiti Miyahara <daiti.miyahara@hotmail.com>
2022-10-18 20:40:17 -0300

    Inclusao de tema bootstrap-lumen

---
+++ Vidly_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_MVC.Startup))]
+namespace Vidly_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
